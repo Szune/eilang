b5f3530e30524733197d7bf41de889166080b104
Szune <erikiwarson@gmail.com>
2019-08-25 16:00:55 +0200

    move astmemberindexerassignment to new file

--- Ast/AstMemberIndexerAssignment.cs
+++ Ast/AstMemberIndexerAssignment.cs
@@ -2,15 +2,17 @@ using System.Collections.Generic;
 
 namespace eilang.Ast
 {
-    public class AstMemberIndexerReference : AstExpression
+    public class AstMemberIndexerAssignment : AstExpression
     {
         public List<string> Identifiers { get; }
         public List<AstExpression> IndexExprs { get; }
+        public AstExpression ValueExpr { get; }
 
-        public AstMemberIndexerReference(List<string> identifiers, List<AstExpression> indexExprs)
+        public AstMemberIndexerAssignment(List<string> identifiers, List<AstExpression> indexExprs, AstExpression valueExpr)
         {
             Identifiers = identifiers;
             IndexExprs = indexExprs;
+            ValueExpr = valueExpr;
         }
 
         public override void Accept(IVisitor visitor, Function function, Module mod)
